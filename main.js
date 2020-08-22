ce6b035571d8705beacf546f7c1f0f4a77b623c8
Marina8989 <marina.mill209@gmail.com>
2020-08-22 14:22:49 -0700

    memory game app updates

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     //define an array of obj with images and name key value pairs
     const cardArray = [
@@ -51,85 +52,96 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     ]
 
- //randomize the images each time the page loads
- cardArray.sort( () => 0.5 - Math.random());
 
-   const grid = document.querySelector('.grid');
-   const resultDisplay = document.querySelector('#result');
-   const restartBtn = document.createElement('button');
 
-   restartBtn.textContent = "Restart";
-  
+     const grid = document.querySelector(".grid");
+     const resultDisplay = document.querySelector("#result");
+     const restartBtn = document.createElement("button");
 
-   // keep track of cards
-    let cardsChosen = [];
-    let cardsChosenId = [];
-    let cardsWon = [];
+     restartBtn.textContent = "Restart";
 
-    // create board and elements, attach them to grid
-    function createBoard() {
-        for(let i = 0; i < cardArray.length; i++) {
-           let card = document.createElement('img');
-           card.setAttribute('src', 'img/cover.jpg');
-           card.setAttribute('data-id', i);
-           card.style.width = '150px';
-           card.style.height = '200px';
-           card.addEventListener('click', flipCard);
-           grid.appendChild(card);
-        }
-    }
-
-    //check for matches
-    function checkForMatch() {
-        let cards = document.querySelectorAll('img');
-        const optionOneId = cardsChosenId[0];
-        const optionTwoId = cardsChosenId[1];
-
-        if(optionOneId == optionTwoId) {
-         cards[optionOneId].setAttribute('src', 'img/cover.jpg');
-         cards[optionTwoId].setAttribute('src', 'img/cover.jpg');
-         alert('You have clicked the same image!');
-        }else if(cardsChosen[0] === cardsChosen[1]) {
-            // alert('You found a match!');
-            cards[optionOneId].setAttribute('src', 'img/done.jpg');
-            cards[optionTwoId].setAttribute('src', 'img/done.jpg');
-            cards[optionOneId].removeEventListener('click', flipCard);
-            cards[optionTwoId].removeEventListener('click', flipCard);
-            cardsWon.push(cardsChosen);
-        }else {
-            cards[optionOneId].setAttribute('src', 'img/cover.jpg');
-            cards[optionTwoId].setAttribute('src', 'img/cover.jpg');
-            // alert('Sorry, try again');
-        }
-        cardsChosen = [];
-        cardsChosenId = [];
-        resultDisplay.textContent = cardsWon.length;
-         if(cardsWon.length === cardArray.length/2) {
-           resultDisplay.textContent = 'You found them all!';
-         } 
-    }
-     
-     //flip card
-    function flipCard() {
-      let cardId = this.getAttribute('data-id');
-      cardsChosen.push(cardArray[cardId].name);
-      cardsChosenId.push(cardId);
-      this.setAttribute('src', cardArray[cardId].img)
-       if(cardsChosenId.length === 2) {
-          setTimeout(checkForMatch, 500);
-       }
-    }
     
-     restartBtn.addEventListener("click", function () {
-       console.log("restarted");
-       cardArray.sort(() => 0.5 - Math.random());
+     //randomize the images each time the page loads
+     cardArray.sort(() => 0.5 - Math.random());
 
+     // keep track of cards
+     let cardsChosen = [];
+     let cardsChosenId = [];
+     let cardsWon = [];
+
+     // create board and elements, attach them to grid
+     function createBoard() {
+       for (let i = 0; i < cardArray.length; i++) {
+         let card = document.createElement("img");
+         card.setAttribute("src", "img/cover.jpg");
+         card.setAttribute("data-id", i);
+         card.style.width = "150px";
+         card.style.height = "200px";
+         card.addEventListener("click", flipCard);
+
+         restartBtn.addEventListener("click", function () {
+           console.log("restarted");
+           cardArray.sort(() => 0.5 - Math.random());
+           card.setAttribute("src", "img/cover.jpg");
+           card.setAttribute("data-id", i);
+           card.style.width = "150px";
+           card.style.height = "200px";
+           card.addEventListener("click", flipCard);
+         });
+
+
+         grid.appendChild(card);
+       }
+        
+     }
+
+     createBoard();
+
+     //check for matches
+     function checkForMatch() {
+       let cards = document.querySelectorAll("img");
+       const optionOneId = cardsChosenId[0];
+       const optionTwoId = cardsChosenId[1];
+
+       if (optionOneId == optionTwoId) {
+         cards[optionOneId].setAttribute("src", "img/cover.jpg");
+         cards[optionTwoId].setAttribute("src", "img/cover.jpg");
+         alert("You have clicked the same image!");
+       } else if (cardsChosen[0] === cardsChosen[1]) {
+         // alert('You found a match!');
+         cards[optionOneId].setAttribute("src", "img/done.jpg");
+         cards[optionTwoId].setAttribute("src", "img/done.jpg");
+         cards[optionOneId].removeEventListener("click", flipCard);
+         cards[optionTwoId].removeEventListener("click", flipCard);
+         cardsWon.push(cardsChosen);
+       } else {
+         cards[optionOneId].setAttribute("src", "img/cover.jpg");
+         cards[optionTwoId].setAttribute("src", "img/cover.jpg");
+         // alert('Sorry, try again');
+       }
        cardsChosen = [];
        cardsChosenId = [];
        resultDisplay.textContent = cardsWon.length;
-       
-     });
-    document.body.append(restartBtn);
-   createBoard();
+       if (cardsWon.length === cardArray.length / 2) {
+         resultDisplay.textContent = "You found them all!";
+       }
+     }
 
+     //flip card
+     function flipCard() {
+       let cardId = this.getAttribute("data-id");
+       cardsChosen.push(cardArray[cardId].name);
+       cardsChosenId.push(cardId);
+       this.setAttribute("src", cardArray[cardId].img);
+       if (cardsChosenId.length === 2) {
+         setTimeout(checkForMatch, 500);
+       }
+     }
+      
+     
+
+     document.body.append(restartBtn);
 })
+
+    
+    
